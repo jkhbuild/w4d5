3d02aa5ddd940b28c281439a225f86b8a7c3bc85
“sajeremy” <9jsantiago4@gmail.com>
2022-10-07 13:48:54 -0400

    question 1

--- Execution_Time_Difference.rb
+++ Execution_Time_Difference.rb
@@ -9,16 +9,45 @@ def my_min(arr)
     end
     min
 end
-p my_min(list)  # =>  -5
+#p my_min(list)  # =>  -5
 
 def my_min_2(arr)
-    min = arr.first
-    arr.each do |i|
-        debugger
-        min = i if arr.all? { |j| j > i }
-        debugger
+    # min = arr.first
+    # arr.each do |i|
+    #     debugger
+    #     min = i if arr.all? { |j| j > i }
+    #     debugger
+    # end
+    # min
+    (0...arr.length).each do |i|
+        count = 0
+        (0...arr.length).each do |j|
+            # debugger
+            count += 1 if arr[i] < arr[j]
+            # debugger
+            return arr[i] if count == arr.length - 1
+
+        end
     end
-    min
+
 end
-p my_min_2(list)  # =>  -5
-            
+#p my_min_2(list)  # =>  -5
+
+list_1 = [5,3,-7]
+list_2 = [2, 3, -6, 7, -6, 7]
+list_3 = [-5, -1, -3]
+
+def largest_contiguos_subsum(list)
+
+    sub_sums = []
+    (0...list.length).each do |i|
+        (0...list.length).each do |j|
+            sub_sums << list[i..j].sum if i > j
+        end     
+    end
+    sub_sums.max
+end
+
+p largest_contiguos_subsum(list_1) #=> 8
+p largest_contiguos_subsum(list_2) #=> 8          
+p largest_contiguos_subsum(list_3) #=> -1
